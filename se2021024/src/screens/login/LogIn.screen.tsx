0f2bcc64c80ea1dde3170089cc2bf103884c4640
Hiruni Ramanayaka <hiruniramanayaka9@example.com>
2025-05-11 20:40:42 +0530

    creage login

--- se2021024/src/screens/login/LogIn.screen.tsx
+++ se2021024/src/screens/login/LogIn.screen.tsx
@@ -1,17 +1,57 @@
-import React from 'react';
-import {View, Text, Button} from 'react-native';
+import React, { useContext, useState } from 'react';
+import {View, Text, Button, TextInput, Alert} from 'react-native';
+import { UserContext } from '../../common/context/userContext';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const LogInScreen = ({navigation}:any) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const {setUser} = useContext(UserContext);
+
+  const handleLogin = async () => {
+    const stored = await AsyncStorage.getItem('user');
+    if(!stored){
+      Alert.alert('No account found. Sign up first.');
+      return;
+    }
+
+    const userData = JSON.parse(stored);
+    if (userData.email === email && userData.password === password) {
+      setUser({ 
+        userName: userData.userName, 
+        email: userData.email, 
+        password: userData.password 
+      });
+      navigation.replace('Home');
+    }else{
+      Alert.alert('Invalid Credentials.')
+    }
+  };
+
   return (
     <View>
         <Text>Log In</Text>
+        <TextInput 
+          placeholder="Email" 
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput 
+          placeholder="Password" 
+          value={password}
+          onChangeText={setPassword}
+        />
         <Button
             title="Log In"
-            onPress={() => {
-                // Navigate to Home screen
-                navigation.navigate('Home');
-            }}
+            onPress={handleLogin}
         />
+        <Text
+          onPress={() => {
+            navigation.navigate('SignUp');
+          }}
+        >
+          Don't have an account? Sign Up
+        </Text>
     </View>
   );
 };
